05aea875e406ff0b39e6cd675fe62d65c15e0c36
Christian Herold <harryherold@googlemail.com>
2014-11-08 14:55:30 +0100

    changed iterative code to object oriented

--- hpc-connect.py
+++ hpc-connect.py
@@ -1,6 +1,8 @@
+__author__ = 'harry'
+
 #!/usr/bin/python3
 import string
-import sys, os, re
+import sys, os, re, getpass, argparse
 from paramiko import SSHClient, SSHConfig, AutoAddPolicy
 from threading import Thread, Event
 
@@ -8,115 +10,56 @@ from threading import Thread, Event
 import time
 
 
-# connect to host
-def connect_to_host(ssh_login):
-    ssh_client = SSHClient()
-    ssh_client.load_system_host_keys()
-    ssh_client.set_missing_host_key_policy(AutoAddPolicy())
-    ssh_client.connect(ssh_login['hostname'], username=ssh_login['user'])
+class SshConnector:
+    host = ""
+    user = ""
+    ssh_config = None
+    ssh_client = None
+    ssh_log = ""
 
-    return ssh_client
+    def __init__(self, host, user=None):
+        if user is None:
+            self.host = host
+            self.read_ssh_config(host)
+            self.connect_with_config()
+        else:
+            self.host = host
+            self.user = user
+            self.connect(host, user)
 
+    # connect to host with config
+    def connect_with_config(self):
+        self.ssh_client = SSHClient()
+        self.ssh_client.load_system_host_keys()
+        self.ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+        self.ssh_client.connect(self.ssh_config['hostname'], username=self.ssh_config['user'])
 
-# read ssh config of user
-def read_ssh_config(hostname):
-    config_file = os.path.join(os.getenv('HOME'), '.ssh/config')
-    config = SSHConfig()
-    config.parse(open(config_file, 'r'))
-    o = config.lookup(hostname)
-    if len(o.keys()) < 2:
-        print("wrong")
-        return None
-    return o
+    # connect with user parameter
+    def connect(self, host, user):
+        password = getpass.getpass()
+        self.ssh_client = SSHClient()
+        self.ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+        self.ssh_client.connect(host, username=user, password=password)
 
+    # read ssh config of user
+    def read_ssh_config(self, hostname):
+        config_file = os.path.join(os.getenv('HOME'), '.ssh/config')
+        config = SSHConfig()
+        config.parse(open(config_file, 'r'))
+        self.ssh_config = config.lookup(hostname)
+        if len(self.ssh_config.keys()) < 2:
+            print("Hostname no found in .ssh/config")
 
-# exec command batch system
-def exec_command(client, command) -> string:
-    ssh_in, ssh_out, ssh_err = client.exec_command(command)
-    if not ssh_err:
-        print('Command execution on host failed\n')
-    return ""
+    # execute command on the host
+    def exec_command(self, cmd):
+        ssh_stdin, ssh_stdout, ssh_stderr = self.ssh_client.exec_command(cmd)
+        self.ssh_log += str(ssh_stderr.read())
+        return str(ssh_stdout.read())
 
+parser = argparse.ArgumentParser()
+parser.add_argument("host", help="host address")
+parser.add_argument("user", help="username")
+args = parser.parse_args()
 
-def get_job(client, node_count, process_count) -> SSHClient:
-    submit_cmd = "salloc -N {0} -n {1} bash\n".format(node_count, process_count)
-    channel = client.invoke_shell()
-    channel.setblocking(1)
-    channel.send(submit_cmd)
-    buf = ''
-    while not "bash-4.1$" in buf:
-        buf += str(channel.recv(9999))
-        time.sleep(10/1000000.0)
-
-    while not channel.send_ready():
-        time.sleep(10/1000000.0)
-
-    host_cmd = "srun hostname\n"
-    channel.send(host_cmd)
-
-
-    buf = ""
-    while not "bash-4.1$" in buf:
-        buf += str(channel.recv(9999))
-        if "bash-4.1$" in buf:
-            break
-
-    # b'srun hostname\r\n'b'taurusi1121\r\ntaurusi1121\r\n'b'bash-4.1$ '
-    return re.findall("taurusi[0-9]{4}", buf)
-
-
-#is better to give the thread the full initialized ssh client
-def run_logger(hostname, nodename, event):
-    config = read_ssh_config(hostname)
-    client = connect_to_host(ssh_config)
-    ssh_in, ssh_out, ssh_err = client.exec_command("hostname")
-    #channel = client.invoke_shell()
-    #channel.send("ssh {0} \'module load gpi2/1.1.0\'".format(nodename))
-    #channel.send("hostname")
-    buf = ssh_out.read()
-    #print("connect to {}".format(nodename))
-    while not event.is_set():
-        print("hallo {0}".format(buf))
-        time.sleep(1)
-        #buf += str(channel.recv(9999))
-        #time.sleep(10/1000000.0)
-    #print(buf)
-    #print("Good Bye")
-    client.close()
-
-host = "taurus"
-ssh_config = read_ssh_config(host)
-logger_client = connect_to_host(ssh_config)
-hpc_hosts = get_job(logger_client, 1, 1)
-print("Got Job")
-#cmd = 'echo "' + '\n'.join(str(i) for i in hpc_hosts) + '" > ~/machinefile'
-#logger_client.exec_command(cmd)
-#logger_client.exec_command("module load gpi2/1.1.0")
-
-stop_event = Event()
-logger_thread = Thread(target=run_logger, args=(host, hpc_hosts[0], stop_event))
-#print(get_worker_nodes(logger_client))
-print("Start logger thread")
-logger_thread.start()
-
-time.sleep(10)
-print("Kill logger thread")
-stop_event.set()
-logger_client.close()
-#ssh_in, ssh_out, ssh_err = logger_client.exec_command("bjobs")
-#print(ssh_out.read())
-#channel = logger_client.invoke_shell()
-#buf = ''
-#while not 'cherold@atlas' in buf:
-#    buf += str(channel.recv(9999))
-
-#print(buf)
-
-#channel.send('bjobs')
-
-#ssh_in, ssh_out, ssh_err = exec_command(logger_client, "id")
-#print(ssh_out.read())
-#ssh_in, ssh_out, ssh_err = exec_command(exec_client, cmd)
-#print(ssh_out.read())
-#exec_client.close()
-
+ssh_connector = SshConnector(args.host, args.user)
+print(ssh_connector.exec_command("ls -l"))
