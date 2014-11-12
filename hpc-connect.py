29350b9982236bff6c372ad2225dc72caed716d3
Christian Herold <harryherold@googlemail.com>
2014-11-12 20:51:56 +0100

    implemented reading config of batch system from file

--- hpc-connect.py
+++ hpc-connect.py
@@ -15,9 +15,12 @@ class SshConnector:
     user = ""
     ssh_config = None
     ssh_client = None
+    batch_sys = None
     ssh_log = ""
 
     def __init__(self, host, user=None):
+        self.batch_sys = Batchsystem()
+        self.batch_sys.read_config()
         if user is None:
             self.host = host
             self.read_ssh_config(host)
@@ -58,9 +61,11 @@ class SshConnector:
 
 
 class Batchsystem:
-    file_name = "batchconf.yaml"
+    batchsystem_config = None
+    config_filename = "batchconf.yaml"
     config_templ = "templconf.yaml"
 
+    # create a new config based on a template
     def create_config(self):
         stream_templ = open(self.config_templ, 'r')
         templ = yaml.load(stream_templ)
@@ -71,19 +76,43 @@ class Batchsystem:
             text = sys.stdin.readline()
             templ[entry] = text.strip()
 
-        stream = open(self.file_name, 'a')
-        yaml.dump(templ, stream)
+        print("Whats the name of the system ?")
+        text = sys.stdin.readline()
+        temp_dict = {text.strip(): templ}
+        stream = open(self.config_filename, 'a')
+        yaml.dump(temp_dict, stream)
+        stream.close()
+        print("Config saved")
+
+    # read the config from the yaml file
+    def read_config(self):
+        stream = open(self.config_filename, 'r')
+        self.batchsystem_config = yaml.load(stream)
         stream.close()
 
+    # return a list of supported batch systems
+    def get_supported_systems(self):
+        return [s for s in self.batchsystem_config]
 
-parser = argparse.ArgumentParser()
-parser.add_argument("host", help="host address")
-parser.add_argument("user", help="username")
-args = parser.parse_args()
+    # print the config of the supported batch systems
+    def print_system(self,):
+        for system in self.batchsystem_config:
+            print("{0} :".format(system))
+            for conf in self.batchsystem_config[system]:
+                print("{:>20}:  {}".format(conf, self.batchsystem_config[system][conf]))
 
-ssh_connector = SshConnector(args.host, args.user)
-print(ssh_connector.exec_command("ls -l"))
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("host", help="host address")
+# parser.add_argument("user", help="username")
+# args = parser.parse_args()
+#
+# ssh_connector = SshConnector(args.host, args.user)
+# print(ssh_connector.exec_command("ls -l"))
 
 b = Batchsystem()
-b.create_config()
+b.read_config()
+b.print_system()
+
+# b.create_config()
 
